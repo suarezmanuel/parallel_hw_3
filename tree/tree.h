53233acfeb0f34b9d217f0e78a4a31e038628bc0
manuelsuarez244@gmail.com <suarezm@planet.cs.biu.ac.il>
2025-01-04 17:05:14 +0200

    done blur

---
+++ tree/tree.h
@@ -0,0 +1,32 @@
+#ifndef BINARY_TREE_H
+#define BINARY_TREE_H
+
+#include <stdbool.h>
+#include <omp.h>
+
+typedef struct TreeNode {
+    int data;
+    struct TreeNode *left;
+    struct TreeNode *right;
+    omp_lock_t lock;
+} TreeNode;
+
+TreeNode* createNode(int data);
+
+TreeNode* insertNode(TreeNode* root, int data);
+
+TreeNode* deleteNode(TreeNode* root, int data);
+
+bool searchNode(TreeNode* root, int data);
+
+TreeNode* findMin(TreeNode* root);
+
+void inorderTraversal(TreeNode* root);
+
+void preorderTraversal(TreeNode* root);
+
+void postorderTraversal(TreeNode* root);
+
+void freeTree(TreeNode* root);
+
+#endif
